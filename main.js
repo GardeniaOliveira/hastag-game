15d9291dd97743da59b1dc0832cd79e7a414c18e
Peter <jobs.gardeniaoliveira@gmail.com>
2023-02-04 12:54:16 +0000

    changed sortLetter

--- main.js
+++ main.js
@@ -12,53 +12,107 @@ const letter = [
     "x", "0"
 ]
 
-const sortLetter = letter[Math.floor(Math.random() * 2)]
-
-
 n1.addEventListener('click', () => {
+    const sortLetter = letter[Math.floor(Math.random() * 2)]
     n1.innerHTML = sortLetter;
-    checkRow()
+    console.log(n1.innerText); 
+    checkWinner()
 })
 
 n2.addEventListener('click', () => {
+    const sortLetter = letter[Math.floor(Math.random() * 2)]
     n2.innerHTML = sortLetter;
-    checkRow()
+    console.log(n2.innerText);
+    checkWinner()
 })
 
 n3.addEventListener('click', () => {
+    const sortLetter = letter[Math.floor(Math.random() * 2)]
     n3.innerHTML = sortLetter;
-    checkRow()
+    console.log(n3.innerText);
+    checkWinner()
 })
 
 n4.addEventListener('click', () => {
+    const sortLetter = letter[Math.floor(Math.random() * 2)]
     n4.innerHTML = sortLetter;
+    checkWinner()
 })
 
 n5.addEventListener('click', () => {
+    const sortLetter = letter[Math.floor(Math.random() * 2)]
     n5.innerHTML = sortLetter;
+    checkWinner()
 })
 
 n6.addEventListener('click', () => {
+    const sortLetter = letter[Math.floor(Math.random() * 2)]
     n6.innerHTML = sortLetter;
+    checkWinner()
 })
 n7.addEventListener('click', () => {
+    const sortLetter = letter[Math.floor(Math.random() * 2)]
     n7.innerHTML = sortLetter;
+    checkWinner()
 })
 
 n8.addEventListener('click', () => {
+    const sortLetter = letter[Math.floor(Math.random() * 2)]
     n8.innerHTML = sortLetter;
+    checkWinner()
 })
 
 n9.addEventListener('click', () => {
+    const sortLetter = letter[Math.floor(Math.random() * 2)]
     n9.innerHTML = sortLetter;
+    checkWinner()
 })
 
-function checkRow(){
+function checkWinner(){
     debugger;
-    if(n1.innerText === n2.innerText === n3.innerText ){
+    if(n1.innerText === n2.innerText === n3.innerText){
         n1.style.backgroundColor = '#7D8DC9';
         n2.style.backgroundColor = '#7D8DC9';
         n3.style.backgroundColor = '#7D8DC9';
+        console.log(n1)
+        console.log(n2)
+        console.log(n3)
     }
-    console.log('passou aqui')
+    else if(n4.innerText === n5.innerText === n6.innerText ){
+        n4.style.backgroundColor = '#7D8DC9';
+        n5.style.backgroundColor = '#7D8DC9';
+        n6.style.backgroundColor = '#7D8DC9';
+        console.log('passou aqui')
+    }
+    else if(n7.innerText === n8.innerText === n9.innerText ){
+        n7.style.backgroundColor = '#7D8DC9';
+        n8.style.backgroundColor = '#7D8DC9';
+        n9.style.backgroundColor = '#7D8DC9';
+        console.log('passou aqui')
+    }
+    else if(n1.innerText === n4.innerText === n7.innerText ){
+        n1.style.backgroundColor = '#7D8DC9';
+        n4.style.backgroundColor = '#7D8DC9';
+        n7.style.backgroundColor = '#7D8DC9';
+        console.log('passou aqui')
+    }
+    else if(n2.innerText === n5.innerText === n8.innerText ){
+        n2.style.backgroundColor = '#7D8DC9';
+        n5.style.backgroundColor = '#7D8DC9';
+        n8.style.backgroundColor = '#7D8DC9';
+        console.log('passou aqui')
+    }
+    else if(n3.innerText === n6.innerText === n9.innerText ){
+        n3.style.backgroundColor = '#7D8DC9';
+        n6.style.backgroundColor = '#7D8DC9';
+        n9.style.backgroundColor = '#7D8DC9';
+        console.log('passou aqui')
+    }
+    else if(n1.innerText === n6.innerText === n9.innerText ){
+        n1.style.backgroundColor = '#7D8DC9';
+        n6.style.backgroundColor = '#7D8DC9';
+        n9.style.backgroundColor = '#7D8DC9';
+        console.log('passou aqui')
+    }
+    
 }
